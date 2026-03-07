b0328c44ba5ff9486b3853187cc429426311a559
Lesly0116 <jeanleslyjocelyn@gmail.com>
2026-03-06 22:19:10 -0500

    Ajout du HomeController pour la navigation

---
+++ src/main/java/com/Lesly/ConvertirFichier/controller/HomeController.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Lesly.ConvertirFichier.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author jeanl
+ */
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        
+        return "index";
+        
+    }
+    
+    @GetMapping("/login")
+    public String login() {
+        
+        return "login"; 
+        
+    }
+    
+    @GetMapping("/inscription")
+    public String register() {
+        
+        return "register";
+        
+    }
+}
